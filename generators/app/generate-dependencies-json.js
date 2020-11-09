10158789f51ad26a40ad244b70a5e9c26524e922
Gabriel Santos <gabrielrsantoss@icloud.com>
2020-11-09 14:38:18 -0200

    Fix: package json project name and author; Refac: dependencies to devDeps and normal dps

--- generators/app/generate-dependencies-json.js
+++ generators/app/generate-dependencies-json.js
@@ -4,13 +4,11 @@ const app_package = JSON.parse(
     fs.readFileSync('generators/app/templates/package.json.template', 'utf8')
 );
 
-console.log(
-    `[DEPS]: found ${Object.keys(app_package.dependencies).length} dependencies`
-);
+const deps = { ...app_package.dependencies, ...app_package.devDependencies };
 
-const dependencies = Object.entries(app_package.dependencies).map(
-    ([k, v]) => `${k}@${v}`
-);
+console.log(`[DEPS]: found ${Object.keys(deps).length} dependencies`);
+
+const dependencies = Object.entries(deps).map(([k, v]) => `${k}@${v}`);
 
 console.log('[DEPS]: Saving to "dependencies.json"');
 
